0a183abd2aa3d0deed8080db18036c5591b929d6
DElAmin <delamin13@gmail.com>
2021-01-24 14:23:06 +0100

    Creating Database Connection

--- _school_record/data.php
+++ _school_record/data.php
@@ -1,3 +1,33 @@
 <?php
-    echo "This is it what you want";
+    include 'db.php';
 ?>
+<tr>
+    <td>1</td>
+    <td>DElAmin</td>
+    <td>Computer</td>
+    <td>500</td>
+    <td>
+        <div class="dropdown">
+            <button class="btn btn-primary" data-toggle="dropdown">Actions <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+                <li><a href="#">Edit</a></li>
+                <li><a href="#">Delete</a></li>
+            </ul>    
+        </div>
+    </td>
+</tr>
+<tr>
+    <td>2</td>
+    <td>Abdou</td>
+    <td>Psychology</td>
+    <td>1000</td>
+    <td>
+        <div class="dropdown">
+            <button class="btn btn-primary" data-toggle="dropdown">Actions <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+                <li><a href="#">Edit</a></li>
+                <li><a href="#">Delete</a></li>
+            </ul>    
+        </div>
+    </td>
+</tr>
